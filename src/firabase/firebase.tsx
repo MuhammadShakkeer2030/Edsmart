90f098d46c716117d565bc5cdd2fbc6ee084f20a
shakkeerz <shakkeerpersonal@gmail.com>
2023-08-09 13:26:01 +0530

    env file added

--- src/firabase/firebase.tsx
+++ src/firabase/firebase.tsx
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoAp15pp3ATlXWVGesfXLZWVuIDGEF57Y",
-  authDomain: "eds-react-fire.firebaseapp.com",
-  projectId: "eds-react-fire",
-  storageBucket: "eds-react-fire.appspot.com",
-  messagingSenderId: "1067213878498",
-  appId: "1:1067213878498:web:dd527598c07fb5a1041abb",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  
 };
 
 const app = initializeApp(firebaseConfig)
